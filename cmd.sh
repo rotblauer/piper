49d6f5b5c30e9f24432b3d3a42083688efae867d
ia <isaac.ardis@gmail.com>
2018-02-13 13:36:21 +0900

    remove test cmd fiels

--- cmd.sh
+++
@@ -1,5 +0,0 @@
-cat /Users/ia/gocode/src/github.com/rotblauer/piper/in.txt \
-|grep -v -E '(Quality|192000|Subprotocol|peer connected)' \
-|grep -v -E '(mismatch|Genesis|shutting)' \
-|grep -v -E '(Accepted|Removed|Added)' \
-|sed '1,4d' \
--- cmd2.sh
+++
@@ -1,3 +0,0 @@
-cat /Users/ia/gocode/src/github.com/rotblauer/piper/in.txt \
-|grep windows \
-|grep Removed \
